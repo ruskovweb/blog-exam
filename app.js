e37cb205651e73abffb0438e80085e0c256f70b5
ruskovweb <ruskovweb@gmail.com>
2022-05-10 01:14:20 +0300

    Add routes and views

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+app.use(express.static("public"));
+
+app.get("/tasks", function(req, res) {
+    res.render("tasks");
+});
+
+app.get("/assessment", function(req, res) {
+    const tasks = [
+        { id: 1, task: "Визуализиране на всички публикации", points: 5 },
+        { id: 2, task: "Визуализиране на страница за създаване на публикация", points: 5 },
+        { id: 3, task: "Създаване на публикация", points: 20 },
+        { id: 4, task: "Визуализиране на страница за детайлите на една публикация", points: 10 },
+        { id: 5, task: "Визуализиране на страница за редактиране на публикация", points: 10 },
+        { id: 6, task: "Редактиране на публикация", points: 35 },
+        { id: 7, task: "Визуализиране на страница за изтриване на публикация", points: 10 },
+        { id: 8, task: "Изтриване на публикация", points: 25 },
+    ];
+
+    const totalPoints = tasks.reduce((acc, value) => acc + value.points, 0);
+
+    const marks = [
+        { points: " 0 - 19", mark: 2 },
+        { points: "20 - 39", mark: 3 },
+        { points: "40 - 59", mark: 4 },
+        { points: "60 - 79", mark: 5 },
+        { points: "80 - 99", mark: 6 },
+        { points: "100 - 120", mark: "6+" },
+    ];
+
+    res.render("assessment", { tasks, marks, totalPoints });
+});
+
 app.listen(PORT, function() {
     console.log(`The server is listening on port ${PORT}`);
 });
--- routes.js
+++ routes.js
@@ -2,6 +2,46 @@ import { Router } from "express";
 
 const router = Router();
 
-// Define your routes here
+let posts   = [];
+let counter = 1;
+
+/*
+    Home page:
+    GET     /
+*/
+router.get("/", function(req, res) {
+    res.render("home", { name: "Dimitar Ruskov" });
+});
+
+/* 
+    Create:
+    GET     /posts/create
+    POST    /posts/create
+*/
+
+/* 
+    Read:
+    GET     /posts/:id
+*/
+
+/* 
+    Update:
+    GET     /posts/edit/:id
+    PATCH   /posts/edit/:id
+*/
+
+/* 
+    Delete:
+    GET     /posts/delete/:id
+    DELETE  /posts/delete/:id
+*/
+
+/* 
+    List:
+    GET     /posts
+*/
+router.get("/posts", function(req, res) {
+    res.render("posts");
+});
 
 export default router;

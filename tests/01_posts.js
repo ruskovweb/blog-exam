a92a6eda9db5db1da79c011fc9e03b970d4c516a
ruskovweb <ruskovweb@gmail.com>
2022-05-11 02:02:36 +0300

    Replace expect with verify

--- tests/01_posts.js
+++ tests/01_posts.js
@@ -5,36 +5,37 @@ describe('All posts', function() {
     it('should contains a table', function(browser) {
         browser.url(`${config.baseUrl}/posts`);
         
-        // Table
-        browser.expect.element('main .container > table').to.be.present.after(1000);
-        browser.expect.element('main .container > table thead tr:nth-child(1) th:nth-child(1)').text.to.equal('ID');
-        browser.expect.element('main .container > table thead tr:nth-child(1) th:nth-child(2)').text.to.equal('Title');
-        browser.expect.element('main .container > table thead tr:nth-child(1) th:nth-child(3)').text.to.equal('Description');
-        browser.expect.element('main .container > table thead tr:nth-child(1) th:nth-child(4)').text.to.equal('Actions');
+        browser.waitForElementPresent('main .container', 1000);
 
-        browser.expect.element('main .container > table tbody tr:nth-child(1) th:nth-child(1)').text.to.equal('1');
-        browser.expect.element('main .container > table tbody tr:nth-child(1) td:nth-child(2)').text.to.equal('This is my first blog post');
-        browser.expect.element('main .container > table tbody tr:nth-child(1) td:nth-child(3)').text.to.equal('Hello and Welcome to my first blog post!');
+        // Table
+        browser.verify.textEquals('main .container > table thead tr:nth-child(1) th:nth-child(1)', 'ID');
+        browser.verify.textEquals('main .container > table thead tr:nth-child(1) th:nth-child(2)', 'Title');
+        browser.verify.textEquals('main .container > table thead tr:nth-child(1) th:nth-child(3)', 'Description');
+        browser.verify.textEquals('main .container > table thead tr:nth-child(1) th:nth-child(4)', 'Actions');
+
+        browser.verify.textEquals('main .container > table tbody tr:nth-child(1) th:nth-child(1)', '1');
+        browser.verify.textEquals('main .container > table tbody tr:nth-child(1) td:nth-child(2)', 'This is my first blog post');
+        browser.verify.textEquals('main .container > table tbody tr:nth-child(1) td:nth-child(3)', 'Hello and Welcome to my first blog post!');
 
         // Create button
-        browser.expect.element('main .container > a.btn.btn-primary').to.be.visible;
-        browser.expect.element('main .container > a:nth-child(1)').text.to.equal('Create');
-        browser.expect.element('main .container > a:nth-child(1)').to.have.attribute('href').which.contains('/posts/create');
+        browser.verify.visible('main .container > a.btn.btn-primary');
+        browser.verify.textEquals('main .container > a:nth-child(1)', 'Create');
+        browser.verify.attributeEquals('main .container > a:nth-child(1)', 'href', '/posts/create');
 
         // Details button
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-info').to.be.visible;
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(1)').text.to.equal('Details');
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(1)').to.have.attribute('href').equals('/posts/details/1');
+        browser.verify.visible('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-info');
+        browser.verify.textEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(1)', 'Details');
+        browser.verify.attributeEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(1)', 'href', '/posts/details/1');
 
         // Edit button
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-warning').to.be.visible;
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(2)').text.to.equal('Edit');
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(2)').to.have.attribute('href').equals('/posts/edit/1');
+        browser.verify.visible('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-warning');
+        browser.verify.textEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(2)', 'Edit');
+        browser.verify.attributeEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(2)', 'href', '/posts/edit/1');
 
         // Delete button
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-danger').to.be.visible;
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(3)').text.to.equal('Delete');
-        browser.expect.element('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(3)').to.have.attribute('href').equals('/posts/delete/1');
+        browser.verify.visible('main .container table tbody tr:nth-child(1) td:nth-child(4) > a.btn.btn-danger');
+        browser.verify.textEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(3)', 'Delete');
+        browser.verify.attributeEquals('main .container table tbody tr:nth-child(1) td:nth-child(4) > a:nth-child(3)', 'href', '/posts/delete/1');
         
         browser.end();
     });
--- tests/02_details.js
+++ tests/02_details.js
@@ -5,15 +5,16 @@ describe('Post details', function() {
     it('should contains a card component with Title, Description and a back button', function(browser) {
         browser.url(`${config.baseUrl}/posts/details/1`);
         
+        browser.waitForElementPresent('main .container', 1000);
+
         // Card
-        browser.expect.element('main .container > .card').to.be.present.after(1000);
-        browser.expect.element('main .container > .card .card-body .card-title').text.to.equal('This is my first blog post');
-        browser.expect.element('main .container > .card .card-body .card-text').text.to.equal('Hello and Welcome to my first blog post!');
+        browser.verify.textEquals('main .container > .card .card-body .card-title', 'This is my first blog post');
+        browser.verify.textEquals('main .container > .card .card-body .card-text', 'Hello and Welcome to my first blog post!');
 
         // Back button
-        browser.expect.element('main .container > .card .card-body a.btn.btn-outline-dark').to.be.visible;
-        browser.expect.element('main .container > .card .card-body a.btn.btn-outline-dark').text.to.equal('Back');
-        browser.expect.element('main .container > .card .card-body a.btn.btn-outline-dark').to.have.attribute('href').equals('/posts');
+        browser.verify.visible('main .container > .card .card-body a.btn.btn-outline-dark');
+        browser.verify.textEquals('main .container > .card .card-body a.btn.btn-outline-dark', 'Back');
+        browser.verify.attributeEquals('main .container > .card .card-body a.btn.btn-outline-dark', 'href', '/posts');
 
         browser.end();
     });
@@ -21,8 +22,8 @@ describe('Post details', function() {
     it('should show error page if the post doesn\'t exist', function(browser) {
         browser.url(`${config.baseUrl}/posts/details/100000`);
         
-        browser.expect.element('main .container > div.alert.alert-danger').to.be.present.after(1000);
-        browser.expect.element('main .container > div.alert.alert-danger').text.to.equal('Post with ID \'100000\' was not found.');
+        browser.waitForElementPresent('main .container > div.alert.alert-danger', 1000);
+        browser.verify.textEquals('main .container > div.alert.alert-danger', 'Post with ID \'100000\' was not found.');
 
         browser.end();
     });
@@ -30,12 +31,12 @@ describe('Post details', function() {
     it('should redirect to all posts when click on the back button', function(browser) {
         browser.url(`${config.baseUrl}/posts/details/1`);
         
-        browser.expect.element('main .container > .card').to.be.present.after(1000);
+        browser.waitForElementPresent('main .container > .card', 1000);
 
         browser.click('main .container > .card .card-body a.btn.btn-outline-dark');
 
-        browser.expect.element('main .container').to.be.present.after(1000);
-        browser.expect.url().to.be.equal(`${config.baseUrl}/posts`);
+        browser.waitForElementPresent('main .container', 1000);
+        browser.verify.urlEquals(`${config.baseUrl}/posts`);
 
         browser.end();
     });

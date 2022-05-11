b8ecc4d00c710f3b203c0c9432b84ecc8d5941d0
stefan.stoyanov <sadas@abv.bg>
2022-05-11 17:41:59 +0300

    my solution

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ let posts = [
 */
 router.get("/", function(req, res) {
     // Replace the 'name' property with your first and last name.
-    res.render("home", { name: "Dimitar Ruskov" });
+    res.render("home", { name: "Stefan Stoyanov" });
 });
 
 /* 
@@ -25,23 +25,95 @@ router.get("/", function(req, res) {
     GET     /posts/create
     POST    /posts/create
 */
+router.get("/posts/create", function(req, res) {
+    res.render("create");
+});
+router.post("/posts/create", function (req, res) {
+    const post = req.body;
+    if(post.title == "" || post.description == ""){
+        res.render("error", {errorMessage:"Post title or description is not valid."});
 
+        return;
+    }
+
+    post.id = counter++;
+    
+    posts.push(post);
+    res.redirect("/posts");
+    
+});
 /* 
     Read:
     GET     /posts/details/:id
 */
+router.get("/posts/details/:id", function(req, res) {
+    const id = Number(req.params.id);
 
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        return res.render("error", {errorMessage:`Post with ID '${id}' was not found.`});
+    }
+    res.render("details", {post});
+
+});
 /* 
     Update:
     GET     /posts/edit/:id
     POST    /posts/edit/:id
 */
+router.get("/posts/edit/:id", function(req, res) {
+    const id = Number(req.params.id);
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        return res.render("error", {errorMessage:`Post with ID '${id}' was not found.`});
+    }
+    res.render("edit", {post});
+});
+router.post("/posts/edit/:id", function (req, res) {
+    const newPost = req.body;
+    if(newPost.title == "" || newPost.description == ""){
+        res.render("error", {errorMessage:"Post title or description is not valid."});
 
+        return;
+    }
+
+    const id = Number(req.params.id);
+
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        return res.render("error", {errorMessage:`Post with ID '${id}' was not found.`});
+    }
+
+    post.title = newPost.title;
+    post.description = newPost.description;
+    res.redirect("/posts");
+
+});
 /* 
     Delete:
     GET     /posts/delete/:id
     POST    /posts/delete/:id
+    
 */
+router.get("/posts/delete/:id", function (req, res) {
+    const id = Number(req.params.id);
+
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        return res.render("error", {errorMessage:`Post with ID '${id}' was not found.`});
+    }
+    res.render("delete", {post});
+});
+router.post("/posts/delete/:id",function (req, res) {
+    const id = Number(req.params.id);
+
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        return res.render("error", {errorMessage:`Post with ID '${id}' was not found.`});
+    }
+    posts = posts.filter(e => e.id != id)
+        res.redirect("/posts");
+});
 
 /* 
     List:

671ec7f21ab380e29b65e5a0802a9b02a362348c
pgmett2022 <pgmett2022@abv.bg>
2022-05-12 04:37:43 +0300

    My solution

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ let posts = [
 */
 router.get("/", function(req, res) {
     // Replace the 'name' property with your first and last name.
-    res.render("home", { name: "Dimitar Ruskov" });
+    res.render("home", { name: "Miroslav Dyakov" });
 });
 
 /* 
@@ -25,29 +25,68 @@ router.get("/", function(req, res) {
     GET     /posts/create
     POST    /posts/create
 */
+router.post("/posts/create", (req, res) => {
+    const post = req.body;
+
+    if(post.title == "" || post.description == ""){
+        res.render("error", {errorMessage: "Post title or description is not valid."})
+        return;
+    }
+    post.id = counter++;
+
+    posts.push(post);
+    res.redirect("/posts");
+})
+
 
 /* 
     Read:
     GET     /posts/details/:id
 */
-
+router.get("/posts/details/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const post = posts.find(e => e.id == id);
+    if(post == undefined){
+        res.render("error", {errorMessage: `Post with ID '${id}' was not found.` });
+        return;
+    }
+    res.render("details", { post });
+    
+})
 /* 
     Update:
     GET     /posts/edit/:id
     POST    /posts/edit/:id
 */
-
+router.get("/posts/edit/:id", function(req, res) {
+    res.render("edit", { posts });
+});
 /* 
     Delete:
     GET     /posts/delete/:id
     POST    /posts/delete/:id
 */
+router.delete("/posts/delete/:id", function(req, res) {
+    const id = Number(req.params.id)
+    
+    const post = posts.find(e => e.id == id);
+    if(post == undefined) {
+        res.sendStatus(404);
+        return;
+
+    }
+    
+    posts = posts.filter(e => e.id !== id);
+    
+    res.render("posts", { posts });
+});
 
 /* 
     List:
     GET     /posts
 */
-router.get("/posts", function(req, res) {
+router.get("/posts", function(req, res)  {
+
     res.render("posts", { posts });
 });
 
